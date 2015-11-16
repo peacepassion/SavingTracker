2a9fd0283228c0295443bf0b7627886bf0b1a8d2
peacepassion <daqi1990@gmail.com>
2015-11-16 20:43:45 +0800

    update loading dialog

--- app/src/main/java/org/peace/savingtracker/ui/widget/ProgressDialog.java
+++ app/src/main/java/org/peace/savingtracker/ui/widget/ProgressDialog.java
@@ -1,43 +1,37 @@
 package org.peace.savingtracker.ui.widget;
 
-import android.app.Dialog;
 import android.content.Context;
 import android.text.TextUtils;
-import android.view.View;
-import android.widget.TextView;
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import org.peace.savingtracker.R;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.afollestad.materialdialogs.Theme;
 
 /**
  * Created by peacepassion on 15/11/12.
  */
-public class ProgressDialog extends Dialog {
+public class ProgressDialog {
 
-  private static final String DEFAULT_MESSAGE = "";
+  private static final String DEFAULT_MESSAGE = "Loading...";
 
-  @Bind(R.id.content) TextView contentTV;
-
-  private String msg;
-
+  private MaterialDialog dialog;
 
   public ProgressDialog(Context context) {
     this(context, DEFAULT_MESSAGE);
   }
 
   public ProgressDialog(Context context, String msg) {
-    super(context, android.R.style.Theme_Holo_Dialog);
-    setContentView(R.layout.dialog_progress);
-    ButterKnife.bind(this, this);
-    this.msg = msg;
-    updateUI();
+    MaterialDialog.Builder builder =
+        new MaterialDialog.Builder(context).theme(Theme.DARK).progress(true, 100);
+    if (!TextUtils.isEmpty(msg)) {
+      builder.content(msg);
+    }
+    dialog = builder.build();
   }
 
-  private void updateUI() {
-    if (TextUtils.isEmpty(msg)) {
-      contentTV.setVisibility(View.GONE);
-    } else {
-      contentTV.setVisibility(View.VISIBLE);
-    }
+  public void show() {
+    dialog.show();
+  }
+
+  public void dismiss() {
+    dialog.dismiss();
   }
 }
