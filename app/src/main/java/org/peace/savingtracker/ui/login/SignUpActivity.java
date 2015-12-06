21a8b820db16181ab92d249deed23d9857e2ec4f
peacepassion <daqi1990@gmail.com>
2015-12-06 12:34:01 +0800

    fix wrong user name after sign up succ

--- app/src/main/java/org/peace/savingtracker/ui/login/SignUpActivity.java
+++ app/src/main/java/org/peace/savingtracker/ui/login/SignUpActivity.java
@@ -9,6 +9,7 @@ import android.widget.EditText;
 import butterknife.Bind;
 import butterknife.OnClick;
 import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVUser;
 import com.avos.avoscloud.SignUpCallback;
 import org.peace.savingtracker.R;
 import org.peace.savingtracker.ui.base.BaseActivity;
@@ -16,6 +17,10 @@ import org.peace.savingtracker.ui.home.HomeActivity;
 import org.peace.savingtracker.ui.widget.ProgressDialog;
 import org.peace.savingtracker.user.User;
 import org.peace.savingtracker.utils.ResUtil;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by peacepassion on 15/11/12.
@@ -52,36 +57,61 @@ public class SignUpActivity extends BaseActivity {
   }
 
   private void doSignUp() {
-    ProgressDialog dlg = new ProgressDialog(this, "Signing up...");
     User user = new User();
     user.setUsername(usernameET.getText().toString());
     user.setEmail(emailET.getText().toString());
     user.setPassword(passwordET.getText().toString());
-    SignUpCallback callback = new SignUpCallback() {
-      @Override public void done(AVException e) {
-        dlg.dismiss();
-        if (e == null) {
-          popHint(ResUtil.getString(R.string.register_success));
-          setResult(RESULT_OK);
-          startActivity(new Intent(SignUpActivity.this, HomeActivity.class));
-          finish();
-        } else {
-          switch (e.getCode()) {
-            case 202:
-              popHint(ResUtil.getString(R.string.error_register_user_name_repeat), true);
-              break;
-            case 203:
-              popHint(ResUtil.getString(R.string.error_register_email_repeat), true);
-              break;
-            default:
-              popHint(ResUtil.getString(R.string.network_error), true);
-              break;
+
+    Observable.create(new Observable.OnSubscribe<Void>() {
+      @Override public void call(Subscriber<? super Void> subscriber) {
+        try {
+          user.signUp();
+          if (!subscriber.isUnsubscribed()) {
+            subscriber.onNext(null);
+            subscriber.onCompleted();
           }
+        } catch (AVException e) {
+          subscriber.onError(e);
         }
       }
-    };
-    dlg.show();
-    user.signUpInBackground(callback);
+    })
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new Subscriber<Void>() {
+          ProgressDialog dlg = new ProgressDialog(SignUpActivity.this, "Signing up...");
+
+          @Override public void onStart() {
+            dlg.show();
+          }
+
+          @Override public void onCompleted() {
+            dlg.dismiss();
+          }
+
+          @Override public void onError(Throwable e) {
+            dlg.dismiss();
+            AVException ae = (AVException) e;
+            switch (ae.getCode()) {
+              case 202:
+                popHint(ResUtil.getString(R.string.error_register_user_name_repeat), true);
+                break;
+              case 203:
+                popHint(ResUtil.getString(R.string.error_register_email_repeat), true);
+                break;
+              default:
+                popHint(ResUtil.getString(R.string.network_error), true);
+                break;
+            }
+          }
+
+          @Override public void onNext(Void aVoid) {
+            user.changeCurrentUser(user, true);
+            popHint(ResUtil.getString(R.string.register_success));
+            setResult(RESULT_OK);
+            startActivity(new Intent(SignUpActivity.this, HomeActivity.class));
+            finish();
+          }
+        });
   }
 
   private boolean validate() {
